b2bf04e214dc704317345b4dd49acdd073dbe174
Tom Bloor <t.bloor@shadowcat.co.uk>
2017-07-27 17:34:25 +0100

    Added admin master template

--- lib/Sesbania/Plugin/Admin/Templates.pm
+++ lib/Sesbania/Plugin/Admin/Templates.pm
@@ -3,8 +3,46 @@ package Sesbania::Plugin::Admin::Templates;
 1;
 __DATA__
 
+@@ layouts/admin.html.ep
+<!DOCTYPE html>
+<html>
+  <head>
+    %= asset 'app.css';
+    <style>
+      body { padding-top: 50px; }
+    </style>
+    <title><%= title %></title>
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Sesbania</a>
+        </div>
+        <div class="collapse navbar-collapse navbar-right" id="nav-menu">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <main class="container">
+      <%= content %>
+    </main>
+    <!-- Javascript at the end as is best practice -->
+    %= asset 'app.js';
+    %= content_for 'javascript';
+  </body>
+</html>
+
 @@ admin/root/login.html.ep
-% layout 'default';
+% layout 'admin';
 % title 'Admin Login';
 <div class="row">
   <div class="col-md-4 col-md-offset-4">
@@ -14,7 +52,7 @@ __DATA__
 </div>
 
 @@ admin/root/index.html.ep
-% layout 'default';
+% layout 'admin';
 % title 'Admin Home';
 <div class="row">
   <div class="col-md-4">
