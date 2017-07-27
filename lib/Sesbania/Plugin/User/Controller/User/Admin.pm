a6375fea4fa02f46e4f26a5a60d2c606977b97ba
Tom Bloor <t.bloor@shadowcat.co.uk>
2017-07-27 19:18:06 +0100

    Improved basic user page

--- lib/Sesbania/Plugin/User/Controller/User/Admin.pm
+++ lib/Sesbania/Plugin/User/Controller/User/Admin.pm
@@ -7,11 +7,51 @@ has resultset => sub {
 
 sub list {
   my $c = shift;
-  $c->stash( users => $c->resultset );
+  $c->stash( users => $c->resultset, user_create_form => {
+    type => 'inline',
+    id => 'user_create_form',
+    action => $c->url_for,
+    method => 'post',
+    inputs => [
+      {
+        label => 'Username',
+        name => 'username',
+        type => 'text',
+        placeholder => 'Username',
+        validator => 'required',
+      },
+      {
+        label => 'Password',
+        name => 'password',
+        type => 'password',
+        placeholder => 'Password',
+        validator => 'required',
+      },
+    ],
+    submit => {
+      label => 'Add User',
+      style => 'primary',
+      size => 'inline',
+    },
+  } );
 }
 
 sub create {
   my $c = shift;
+
+  my $validation = $c->validation;
+  $validation->required('username')->not_in_resultset('username', $c->resultset);
+  $validation->required('password');
+
+  if ( $validation->has_error ) {
+    $c->flash( error => "Error creating user" );
+  } else {
+    $c->resultset->create({
+      map { $_ => $validation->param($_) } @{ $validation->passed },
+    });
+    $c->flash( success => "User Created" );
+  }
+  $c->redirect_to( '/admin/users' );
 }
 
 sub read {
--- lib/Sesbania/Plugin/User/Templates.pm
+++ lib/Sesbania/Plugin/User/Templates.pm
@@ -6,4 +6,17 @@ __DATA__
 @@ user/admin/list.html.ep
 % layout 'admin_full';
 % title 'User Admin';
-<h2>User Admin</h2>
+<h1>User Admin</h1>
+% if ( my $error = flash 'error' ) {
+  <div class="alert alert-danger" role="alert">
+  <strong>Error!</strong> <%= $error %>
+  </div>
+% } elsif ( my $success = flash 'success' ) {
+  <div class="alert alert-success" role="alert">
+  <strong>Success!</strong> <%= $success %>
+  </div>
+% }
+%= sesbania_form_builder $user_create_form
+% for my $user ( $users->all ) {
+<p><%= $user->username %></p>
+% }
